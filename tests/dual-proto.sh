22cc82d21d4eae15d52d062832ce7631223f24b2
Kyle Manna <kyle@kylemanna.com>
2016-06-01 13:45:48 -0700

    tests: dual-proto: Fail gracefully on firewall
    * Firewall was already configured, don't exit the unit test.

--- tests/dual-proto.sh
+++ tests/dual-proto.sh
@@ -36,8 +36,8 @@ docker run --volumes-from $OVPN_DATA --rm $IMG ovpn_listclients | grep $CLIENT_U
 #
 # Fire up the server
 #
-sudo iptables -N DOCKER
-sudo iptables -I FORWARD -j DOCKER
+sudo iptables -N DOCKER || echo 'Firewall already configured'
+sudo iptables -I FORWARD -j DOCKER || echo 'Forward already configured'
 
 # run in shell bg to get logs
 docker run --name "ovpn-test-udp" --volumes-from $OVPN_DATA --rm -p 1194:1194/udp --privileged $IMG &
