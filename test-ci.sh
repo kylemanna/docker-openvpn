e8eb47ef2bf67bd3b85771bbb1c157b0d81f0fbe
Kyle Manna <kyle@kylemanna.com>
2016-06-01 14:20:31 -0700

    test: Add a simple completion message and test cnt
    * Simplify the review of the test log by looking for a magic line.

--- test-ci.sh
+++ test-ci.sh
@@ -3,13 +3,17 @@
 set -x
 
 cd "$(dirname "$(readlink -f "$0")")/tests"
+let cnt=0
 
 for i in *.sh; do
-    echo -e "\n>> Running test $i\n"
+    cnt=$(($cnt + 1))
+    echo -e "\n>> Running test #$cnt \"$i\"\n"
     ./${i}
     retval=$?
     if [ $retval != 0 ]; then
-        echo "Failed $i with exit code $retval"
+        echo ">> FAILED test #$cnt \"$i\", exit code $retval"
         exit $retval
     fi
 done
+
+echo ">> All $cnt tests PASSED"
