78c509be9616bde089795a6606dd0ca406f084af
probaaho <probaaho@gmail.com>
2021-10-06 03:30:16 +0200

    set name and run docker with restart

--- start_docker.sh
+++ start_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 OVPN_DATA=$1
 
-docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
+docker run --restart unless-stopped --stop-timeout 300 -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --name ovpn-probaaho --cap-add=NET_ADMIN kylemanna/openvpn
 
